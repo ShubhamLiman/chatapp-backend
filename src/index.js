f5b4405eb40e177e0e0b726eed40df7bf4277b42
Shubham Liman <shubhamlstudymail@gmail.com>
2025-02-18 11:03:04 +0530

    -

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "https://chatapp-frontend-rust.vercel.app",
-      "http://localhost:5173/",
+      "http://localhost:5173",
     ], // Allow requests from all origins
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
