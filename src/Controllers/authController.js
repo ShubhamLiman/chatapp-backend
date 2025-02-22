b8b7c16a34d471b703ab67993775e7933907dd73
Shubham Liman <shubhamlstudymail@gmail.com>
2025-02-22 13:21:58 +0530

    -

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -28,13 +28,13 @@ export const signup = async (req, res) => {
     const user = await registerUser({ fullName, email, password });
 
     if (!user.success) {
-      return res.status(400).json(user);
+      return res.status(400).json({ success: false, user });
     }
 
     res.status(201).json(user);
   } catch (error) {
     console.log("Error in signup controller", error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
@@ -42,13 +42,15 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     if (!email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
     }
 
     const user = await loginUser({ email, password });
 
     if (!user.success) {
-      return res.status(400).json({ message: user.message });
+      return res.status(400).json({ success: false, message: user.message });
     }
 
     const token = generateToken(user.user._id);
@@ -56,17 +58,17 @@ export const login = async (req, res) => {
     res.status(200).json({ user, token });
   } catch (error) {
     console.log("Error in login controller", error.message);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
 export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logged out successfully" });
+    res.status(200).json({ success: true, message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
@@ -75,18 +77,22 @@ export const changeProfilePic = async (req, res) => {
     const { profilePic } = req.body;
     const userId = req.user._id;
     if (!userId) {
-      return res.status(400).json({ message: "Login to continue" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Login to continue" });
     }
     if (!profilePic) {
-      return res.status(400).json({ message: "Profile pic is required" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Profile pic is required" });
     }
     const upload = await updateProfilepic(profilePic, userId);
     if (!upload.success) {
-      res.status(400).json(upload);
+      res.status(400).json({ success: false, upload });
     }
-    res.status(200).json(upload);
+    res.status(200).json({ success: true, upload });
   } catch (error) {
     console.log("error in update profile:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
