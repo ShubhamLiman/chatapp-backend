b10f2331d14afbcfa4509616ba4c0f08766aa158
Shubham Liman <shubhamlstudymail@gmail.com>
2025-03-07 10:54:40 +0530

    -

--- src/Middlewares/jwtconfig.js
+++ src/Middlewares/jwtconfig.js
@@ -14,8 +14,8 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
       maxAge: 1 * 24 * 60 * 60 * 1000, // MS
       httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-      sameSite: "lax", // CSRF attacks cross-site request forgery attacks
-      secure: false,
+      sameSite: "none", // CSRF attacks cross-site request forgery attacks
+      secure: process.env.NODE_ENV !== "development",
     });
   } catch (err) {
     console.log("Error in setting cookie: ", err.message);
